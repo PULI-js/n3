47a6c16718ab8a033a3a88ab442815aff4e6bf19
PULI-js <129178672+PULI-js@users.noreply.github.com>
2023-03-28 23:47:41 +0530

    Add files via upload

---
+++ models/book.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Book = mongoose.Schema({
+  ISBN: { type: Number, required: true, unique: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  isInStock: {type: Boolean, required: true},
+  __v:{ type: Number},
+  edition: Number,
+  printDate: Date,
+});
+
+module.exports = mongoose.model('Book', Book);
